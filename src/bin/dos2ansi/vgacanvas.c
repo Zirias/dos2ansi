d2c3d6618a64cb18a31a1db159778dbd231653d3
Felix Palmen <felix@palmen-it.de>
2024-01-20 13:05:04 +0100

    VgaCanvas: Implement setters and actions
    Implement methods for setting colors, moving the cursor and writing a
    character to the canvas.

--- src/bin/dos2ansi/vgacanvas.c
+++ src/bin/dos2ansi/vgacanvas.c
@@ -15,13 +15,13 @@ typedef struct VgaChar
 
 struct VgaCanvas
 {
-    uint8_t att;
     size_t width;
     size_t height;
     size_t linecapa;
     size_t x;
     size_t y;
-    VgaChar *lines[];
+    VgaChar **lines;
+    uint8_t att;
 };
 
 static VgaChar *createline(int width)
@@ -35,25 +35,111 @@ static VgaChar *createline(int width)
     return line;
 }
 
-static void createlines(VgaCanvas *self, size_t oldcapa)
+static void expand(VgaCanvas *self)
 {
-    for (size_t i = oldcapa; i < self->linecapa; ++i)
+    if (self->y >= self->linecapa)
     {
-	self->lines[i] = createline(self->width);
+	size_t oldcapa = self->linecapa;
+	if (self->y - self->linecapa < LINESCHUNK)
+	    self->linecapa += LINESCHUNK;
+	else self->linecapa = self->y+1;
+	self->lines = xrealloc(self->lines,
+		self->linecapa * sizeof *self->lines);
+	for (size_t i = oldcapa; i < self->linecapa; ++i)
+	{
+	    self->lines[i] = createline(self->width);
+	}
     }
 }
 
 VgaCanvas *VgaCanvas_create(int width)
 {
-    VgaCanvas *self = xmalloc(sizeof *self + LINESCHUNK * sizeof *self->lines);
-    self->att = 0x07;
+    VgaCanvas *self = xmalloc(sizeof *self);
     self->width = width;
     self->height = 0;
-    self->linecapa = LINESCHUNK;
+    self->linecapa = 0;
     self->x = 0;
     self->y = 0;
-    createlines(self, 0);
+    self->lines = 0;
+    self->att = 0x07;
+    expand(self);
     return self;
+}
+
+void VgaCanvas_put(VgaCanvas *self, char c)
+{
+    if (c == 0x0a) self->x = 0;
+    else if (c == 0x0d) VgaCanvas_down(self, 1);
+    else if ((unsigned char)c >= 0x20U)
+    {
+	if (self->x == self->width)
+	{
+	    self->x = 0;
+	    VgaCanvas_down(self, 1);
+	}
+	self->lines[self->y][self->x].att = self->att;
+	self->lines[self->y][self->x].chr = c;
+	++self->x;
+	if (self->y >= self->height) self->height = self->y+1;
+    }
+}
+
+void VgaCanvas_setFg(VgaCanvas *self, char fg)
+{
+    self->att &= 0xf0U;
+    self->att |= (unsigned char)fg & 0xfU;
+}
+
+void VgaCanvas_setBg(VgaCanvas *self, char bg)
+{
+    self->att &= 0xfU;
+    self->att |= ((unsigned char)bg & 0xfU) << 4;
+}
+
+void VgaCanvas_up(VgaCanvas *self, unsigned n)
+{
+    if (!n) return;
+    if (n >= self->y) self->y = 0;
+    else self->y -= n;
+}
+
+void VgaCanvas_down(VgaCanvas *self, unsigned n)
+{
+    if (!n) return;
+    self->y += n;
+    expand(self);
+}
+
+void VgaCanvas_left(VgaCanvas *self, unsigned n)
+{
+    if (!n) return;
+    if (self->x == self->width)
+    {
+	self->x = 0;
+	++self->y;
+    }
+    if (n > self->x)
+    {
+	VgaCanvas_up(self, (n - self->x) / self->width + 1);
+	n %= self->width;
+	self->x += self->width;
+    }
+    self->x -= n;
+}
+
+void VgaCanvas_right(VgaCanvas *self, unsigned n)
+{
+    if (self->x == self->width)
+    {
+	self->x = 0;
+	VgaCanvas_down(self, 1);
+    }
+    if (n > self->width - self->x)
+    {
+	VgaCanvas_down(self, (n - (self->width - self->x)) / self->width + 1);
+	self->x = (self->x + n) % self->width;
+    }
+    else self->x += n;
 }
 
 void VgaCanvas_destroy(VgaCanvas *self)
--- src/bin/dos2ansi/vgacanvas.h
+++ src/bin/dos2ansi/vgacanvas.h
@@ -10,11 +10,10 @@ VgaCanvas *VgaCanvas_create(int width);
 void VgaCanvas_put(VgaCanvas *self, char c);
 void VgaCanvas_setFg(VgaCanvas *self, char fg);
 void VgaCanvas_setBg(VgaCanvas *self, char bg);
-void VgaCanvas_setXY(VgaCanvas *self, char x, char y);
-void VgaCanvas_up(VgaCanvas *self, char n);
-void VgaCanvas_down(VgaCanvas *self, char n);
-void VgaCanvas_left(VgaCanvas *self, char n);
-void VgaCanvas_right(VgaCanvas *self, char n);
+void VgaCanvas_up(VgaCanvas *self, unsigned n);
+void VgaCanvas_down(VgaCanvas *self, unsigned n);
+void VgaCanvas_left(VgaCanvas *self, unsigned n);
+void VgaCanvas_right(VgaCanvas *self, unsigned n);
 
 int VgaCanvas_x(const VgaCanvas *self);
 int VgaCanvas_y(const VgaCanvas *self);
