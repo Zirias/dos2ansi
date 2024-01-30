938e64b9b913bc8537cf214e1d8905f12d940785
Felix Palmen <felix@palmen-it.de>
2024-01-30 11:18:26 +0100

    VgaCanvas: document serialize()
    As VgaCanvas_serialize() writes a non-obvious format to its output
    Stream (which a StreamWriter needs to handle), some explicit
    documentation is required here.

--- src/bin/dos2ansi/vgacanvas.h
+++ src/bin/dos2ansi/vgacanvas.h
@@ -30,6 +30,19 @@ void VgaCanvas_down(VgaCanvas *self, unsigned n);
 void VgaCanvas_left(VgaCanvas *self, unsigned n);
 void VgaCanvas_right(VgaCanvas *self, unsigned n);
 
+/* Serialize the canvas contents as a stream of uint16_t values containing
+ * characters of the Unicode BMP in machine byte order, using a supplied
+ * codepage for the character mapping.
+ *
+ * Attribute changes are encoded using Private Use Area codepoints U+EE00 to
+ * U+EEFF with the low byte containing the new attribute where the low nibble
+ * contains the foreground color and the high nibble the background color,
+ * encoded according to ANSI with intensity bit. Codepoint U+EF00 is used to
+ * reset all attributes.
+ *
+ * An output stream is expected to interpret these codepoints. It only needs
+ * to accept a single uint16_t at a time.
+ */
 int VgaCanvas_serialize(const VgaCanvas *self,
 	Stream *out, const Codepage *cp, VgaSerFlags flags);
 
