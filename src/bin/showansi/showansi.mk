e20b141d0ba9ccb77a310c0c5f385b1f4a61dbf2
Felix Palmen <felix@palmen-it.de>
2024-02-17 12:48:51 +0100

    showansi: [SECURITY] Hardcode dos2ansi path
    The showansi script uses some "eval magic" partially based on output of
    dos2ansi. This could allow execution of arbitrary code when an attacker
    could trick it into executing some malicious dos2ansi imposter instead.
    
    Hardcoding the path to dos2ansi makes this significantly harder, as an
    attacker would need to replace the binary installed there.

--- src/bin/showansi/showansi.mk
+++ src/bin/showansi/showansi.mk
@@ -1,5 +1,8 @@
-# disable building and stripping:
-showansi_TGTDIR=	$(showansi_SRCDIR)
-showansi_STRIPWITH:=
+showansi_NOBUILD:=	1
+
+_dos2ansi_name:=	$(dos2ansi_TARGET)
+_dos2ansi_dir:=		$($(dos2ansi_INSTALLDIRNAME)dir)
+showansi_SUB_FILES:=	showansi
+showansi_SUB_LIST:=	"DOS2ANSI=$(_dos2ansi_dir)$(PSEP)$(_dos2ansi_name)"
 
 $(call binrules, showansi)
