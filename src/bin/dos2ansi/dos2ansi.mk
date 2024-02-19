26e4decda31d6f84171e51727a01ef013e53aadf
Felix Palmen <felix@palmen-it.de>
2024-02-19 08:03:25 +0100

    dos2ansi.mk: Don't always sub win-specific files
    Only add Windows-specific files to _SUB_FILES when actually building for
    Windows.

--- src/bin/dos2ansi/dos2ansi.mk
+++ src/bin/dos2ansi/dos2ansi.mk
@@ -15,10 +15,7 @@ dos2ansi_MODULES:=	ansicolorwriter \
 			vgacanvas
 
 dos2ansi_VERSION:=	1.3
-
-dos2ansi_SUB_FILES:=	decl.h \
-			dos2ansi.exe.manifest \
-			windres.rc
+dos2ansi_SUB_FILES:=	decl.h
 
 ifeq ($(PLATFORM),posix)
 ifeq ($(WITH_CURSES),1)
@@ -32,6 +29,8 @@ ifeq ($(PLATFORM),win32)
 dos2ansi_MODULES+=	winconsolewriter
 dos2ansi_win32_RES:=	windres
 dos2ansi_DEFINES+=	-DOSNAME=\"Windows\"
+dos2ansi_SUB_FILES+=	dos2ansi.exe.manifest \
+			windres.rc
 else
 ifeq ($(CROSS_COMPILE),)
 dos2ansi_DEFINES+=	-DOSNAME=\"$(SYSNAME)\"
