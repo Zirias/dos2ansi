aa47a491d471114d94ffe481ba20d4270d479f3c
Felix Palmen <felix@palmen-it.de>
2024-02-20 10:47:25 +0100

    AnsiSysRenderer: Set canvas width for text screen
    Set canvas width to 40 or 80 according to the text screen mode (modes
    0-3) requested.

--- src/bin/dos2ansi/ansisysrenderer.c
+++ src/bin/dos2ansi/ansisysrenderer.c
@@ -104,7 +104,22 @@ int AnsiSysRenderer_render(VgaCanvas *canvas, Stream *stream)
 		}
 		else if (c == 'h' || c == 'l')
 		{
-		    if (escargs[0] == 7) VgaCanvas_setWrap(canvas, c == 'h');
+		    switch (escargs[0])
+		    {
+			case 0:
+			case 1:
+			    VgaCanvas_reset(canvas, 40);
+			    break;
+			case 2:
+			case 3:
+			    VgaCanvas_reset(canvas, 80);
+			    break;
+			case 7:
+			    VgaCanvas_setWrap(canvas, c == 'h');
+			    break;
+			default:
+			    break;
+		    }
 		    esc = 0;
 		}
 		else
