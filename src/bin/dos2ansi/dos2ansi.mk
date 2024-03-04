d49ecf89b70154d0567146660741b9f2510d3efb
Felix Palmen <felix@palmen-it.de>
2024-03-04 17:13:15 +0100

    Config: Use generated help and usage text
    Add mkclidoc submodule and dos2ansi.cdoc to process into help and usage
    messages. This prepares also adding a manpage.

--- src/bin/dos2ansi/dos2ansi.mk
+++ src/bin/dos2ansi/dos2ansi.mk
@@ -42,3 +42,23 @@ dos2ansi_DEFINES+=	-DFORCE_STDIO
 endif
 
 $(call binrules, dos2ansi)
+
+MKCLIDOCDIR=	tools$(PSEP)mkclidoc
+MKCLIDOC=	$(MKCLIDOCDIR)$(PSEP)dist$(PSEP)mkclidoc$(EXE)
+HELP_H=		$(dos2ansi_SRCDIR)$(PSEP)help.h
+D2A_CDOC=	$(dos2ansi_SRCDIR)$(PSEP)dos2ansi.cdoc
+
+$(HELP_H): $(D2A_CDOC) $(MKCLIDOC)
+	@$(VGEN)
+	@$(VR) $(MKCLIDOC) -fcpp -o "$@" "$<"
+
+dos2ansi_sub:	$(HELP_H)
+
+$(MKCLIDOC):
+	+@$(MAKE) -C $(MKCLIDOCDIR) GIT=$(GIT) PORTABLE=1 install
+
+CLEAN += $(MKCLIDOC) $(HELP_H)
+
+clean::
+	+@$(MAKE) -C $(MKCLIDOCDIR) distclean
+
